c643bbecb8f39cce14842edc7aa2fd4f4d644c8d
sunil.jagtap <sunil.jagtap@PTS-L0109>
2023-03-11 00:36:34 +0530

    Variables

---
+++ src/com/day2/VariablesExamples.java
@@ -0,0 +1,26 @@
+package com.day2;
+
+public class VariablesExamples {
+                public static void main(String[] args) {
+	int ab=5;
+	short bc=6;
+	boolean mycondition = true ;
+	char Gender = 'M';
+	double y = 3.12 ;
+	
+	System.out.println(ab);
+	System.out.println(bc);
+	System.out.println(y);
+	System.out.println(mycondition);
+	System.out.println(Gender);
+
+                
+                }
+
+                 public Object jay (){ Object a = null;
+				return a ;
+}
+
+
+}
+
