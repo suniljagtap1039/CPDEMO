a6567a8b53d41c362c5a6a13be4577d2d592f7f7
sunil.jagtap <sunil.jagtap@PTS-L0109>
2023-03-11 01:28:51 +0530

    Student variables

---
+++ src/com/day2/Student.java
@@ -0,0 +1,19 @@
+package com.day2;
+
+public class Student {
+public static void main(String[] args) {
+	String Name = "Sunil";
+	int rollno = 880 ;
+	String Email = "suniljagtap9`97@gmail.com";
+	double marks = 300.55;
+	double percentage = 70.7;
+	
+	
+	System.out.println("Student Name is :" + Name);
+	System.out.println("Student Roll NO  is :" + rollno);
+	System.out.println("Student Email is :" + Email );
+	System.out.println("Student marks is :" + marks);
+	System.out.println("Student percentage is :" + percentage);
+	
+}
+}
